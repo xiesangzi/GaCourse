2d5e0cd40432ffff3000447a6a425fe1064d35f5
xiesangzi <1358398521@qq.com>
2019-10-14 16:34:45 +0800

    优化算法

--- src/main/java/com/github/gacourse/GaCourseApplication.java
+++ src/main/java/com/github/gacourse/GaCourseApplication.java
@@ -1,7 +1,19 @@
 package com.github.gacourse;
 
+import io.undertow.Undertow;
+import io.undertow.servlet.api.DeploymentInfo;
+import io.undertow.servlet.api.SecurityConstraint;
+import io.undertow.servlet.api.SecurityInfo;
+import io.undertow.servlet.api.TransportGuaranteeType;
+import io.undertow.servlet.api.WebResourceCollection;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.undertow.UndertowBuilderCustomizer;
+import org.springframework.boot.web.embedded.undertow.UndertowDeploymentInfoCustomizer;
+import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GaCourseApplication {
@@ -10,4 +22,32 @@ public class GaCourseApplication {
         SpringApplication.run(GaCourseApplication.class, args);
     }
 
+    /**
+     * https服务端口
+     */
+    @Value("${server.port}")
+    private Integer httpsPort;
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        UndertowServletWebServerFactory serverFactory = new UndertowServletWebServerFactory();
+        serverFactory.addBuilderCustomizers(new UndertowBuilderCustomizer() {
+            @Override
+            public void customize(Undertow.Builder builder) {
+                builder.addHttpListener(httpsPort + 1, "0.0.0.0");
+            }
+        });
+        // 开启HTTP自动跳转至HTTPS
+        serverFactory.addDeploymentInfoCustomizers(new UndertowDeploymentInfoCustomizer() {
+            @Override
+            public void customize(DeploymentInfo deploymentInfo) {
+                deploymentInfo.addSecurityConstraint(new SecurityConstraint()
+                        .addWebResourceCollection(new WebResourceCollection().addUrlPattern("/*"))
+                        .setTransportGuaranteeType(TransportGuaranteeType.CONFIDENTIAL)
+                        .setEmptyRoleSemantic(SecurityInfo.EmptyRoleSemantic.PERMIT))
+                        .setConfidentialPortManager(exchange -> httpsPort);
+            }
+        });
+        return serverFactory;
+    }
 }
--- src/test/java/com/github/gacourse/GaCourseApplicationTests.java
+++ src/test/java/com/github/gacourse/GaCourseApplicationTests.java
@@ -11,6 +11,6 @@ public class GaCourseApplicationTests {
 
     @Test
     public void contextLoads() {
-    }
 
+    }
 }
